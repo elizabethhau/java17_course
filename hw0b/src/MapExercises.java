6764592b4c25488789759423a3b4ea396aeeabf1
Elizabeth Hau <elizabeth.h.hau@gmail.com>
2024-08-08 21:28:57 -0400

    finish MapExercises in hw0b

--- hw0b/src/MapExercises.java
+++ hw0b/src/MapExercises.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,7 +8,14 @@ public class MapExercises {
      */
     public static Map<Character, Integer> letterToNum() {
         // TODO: Fill in this function.
-        return null;
+        Map<Character, Integer> output = new HashMap<>();
+        int i = 1;
+        char[] alphabets = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        for (char c : alphabets) {
+            output.put(c, i);
+            i++;
+        }
+        return output;
     }
 
     /** Returns a map from the integers in the list to their squares. For example, if the input list
@@ -15,12 +23,26 @@ public class MapExercises {
      */
     public static Map<Integer, Integer> squares(List<Integer> nums) {
         // TODO: Fill in this function.
-        return null;
+        Map<Integer, Integer> squaresMap = new HashMap<>();
+        for (int i : nums) {
+            squaresMap.put(i, (int)Math.pow(i, 2));
+        }
+        return squaresMap;
     }
 
     /** Returns a map of the counts of all words that appear in a list of words. */
     public static Map<String, Integer> countWords(List<String> words) {
         // TODO: Fill in this function.
-        return null;
+        Map<String, Integer> countMap = new HashMap<>();
+        for (String w : words) {
+            if (countMap.containsKey(w)) {
+                int count = countMap.get(w);
+                count++;
+                countMap.put(w, count);
+            } else {
+                countMap.put(w, 1);
+            }
+        }
+        return countMap;
     }
 }

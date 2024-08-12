625776c61103b5c4d97bffcd75b276f27b405d33
Elizabeth Hau <elizabeth.h.hau@gmail.com>
2024-08-11 22:03:33 -0400

    Created Dessert class

--- hw0b/src/Dessert.java
+++ hw0b/src/Dessert.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Dessert {
+public class Dessert {
+  public int flavor;
+  public int price;
+  public static int numDesserts;
+  public Dessert(int flavor, int price) {
+    this.flavor = flavor;
+    this.price = price;
+    numDesserts++;
+  }
+
+  public void printDessert() {
+    System.out.println(this.flavor + " " + this.price + " " + numDesserts);
+  }
+
+  public static void main(String[] args) {
+    System.out.println("I love dessert!");
+  }
 }

20a362a513c88c97d145e4f854a447b491b81362
Elizabeth Hau <elizabeth.h.hau@gmail.com>
2024-09-11 00:37:11 -0400

    Finished project 0

--- proj0/src/game2048logic/Model.java
+++ proj0/src/game2048logic/Model.java
@@ -9,248 +9,309 @@ import java.util.Formatter;
 import java.util.List;
 
 
-/** The state of a game of 2048.
- *  @author P. N. Hilfinger + Josh Hug
+/**
+ * The state of a game of 2048.
+ *
+ * @author P. N. Hilfinger + Josh Hug
  */
 public class Model {
-    /** Current contents of the board. */
-    private final Board board;
-    /** Current score. */
-    private int score;
+  /**
+   * Current contents of the board.
+   */
+  private final Board board;
+  /**
+   * Current score.
+   */
+  private int score;
 
-    private boolean maxTileExists = false;
+  private boolean maxTileExists = false;
 
-    /* Coordinate System: column x, row y of the board (where x = 0,
-     * y = 0 is the lower-left corner of the board) will correspond
-     * to board.tile(x, y).  Be careful!
-     */
+  /* Coordinate System: column x, row y of the board (where x = 0,
+   * y = 0 is the lower-left corner of the board) will correspond
+   * to board.tile(x, y).  Be careful!
+   */
 
-    /** Largest piece value. */
-    public static final int MAX_PIECE = 2048;
+  /**
+   * Largest piece value.
+   */
+  public static final int MAX_PIECE = 2048;
 
-    /** A new 2048 game on a board of size SIZE with no pieces
-     *  and score 0. */
-    public Model(int size) {
-        board = new Board(size);
-        score = 0;
-    }
+  /**
+   * A new 2048 game on a board of size SIZE with no pieces
+   * and score 0.
+   */
+  public Model(int size) {
+    board = new Board(size);
+    score = 0;
+  }
 
-    /** A new 2048 game where RAWVALUES contain the values of the tiles
-     * (0 if null). VALUES is indexed by (x, y) with (0, 0) corresponding
-     * to the bottom-left corner. Used for testing purposes. */
-    public Model(int[][] rawValues, int score) {
-        board = new Board(rawValues);
-        this.score = score;
-    }
+  /**
+   * A new 2048 game where RAWVALUES contain the values of the tiles
+   * (0 if null). VALUES is indexed by (x, y) with (0, 0) corresponding
+   * to the bottom-left corner. Used for testing purposes.
+   */
+  public Model(int[][] rawValues, int score) {
+    board = new Board(rawValues);
+    this.score = score;
+  }
 
-    /** Return the current Tile at (x, y), where 0 <= x < size(),
-     *  0 <= y < size(). Returns null if there is no tile there.
-     *  Used for testing. */
-    public Tile tile(int x, int y) {
-        return board.tile(x, y);
-    }
+  /**
+   * Return the current Tile at (x, y), where 0 <= x < size(),
+   * 0 <= y < size(). Returns null if there is no tile there.
+   * Used for testing.
+   */
+  public Tile tile(int x, int y) {
+    return board.tile(x, y);
+  }
 
-    /** Return the number of squares on one side of the board. */
-    public int size() {
-        return board.size();
-    }
+  /**
+   * Return the number of squares on one side of the board.
+   */
+  public int size() {
+    return board.size();
+  }
 
-    /** Return the current score. */
-    public int score() {
-        return score;
-    }
+  /**
+   * Return the current score.
+   */
+  public int score() {
+    return score;
+  }
 
-    private void setMaxTileExists(boolean exists) {
-        this.maxTileExists = exists;
-    }
+  private void setMaxTileExists(boolean exists) {
+    this.maxTileExists = exists;
+  }
 
 
-    /** Clear the board to empty and reset the score. */
-    public void clear() {
-        score = 0;
-        board.clear();
-    }
+  /**
+   * Clear the board to empty and reset the score.
+   */
+  public void clear() {
+    score = 0;
+    board.clear();
+  }
 
-    /** Add TILE to the board. There must be no Tile currently at the
-     *  same position. */
-    public void addTile(Tile tile) {
-        board.addTile(tile);
-    }
+  /**
+   * Add TILE to the board. There must be no Tile currently at the
+   * same position.
+   */
+  public void addTile(Tile tile) {
+    board.addTile(tile);
+  }
 
-    /** Return true iff the game is over (there are no moves, or
-     *  there is a tile with value 2048 on the board). */
-    public boolean gameOver() {
-        return maxTileExists() || !atLeastOneMoveExists();
-    }
+  /**
+   * Return true iff the game is over (there are no moves, or
+   * there is a tile with value 2048 on the board).
+   */
+  public boolean gameOver() {
+    return maxTileExists() || !atLeastOneMoveExists();
+  }
 
-    /** Returns this Model's board. */
-    public Board getBoard() {
-        return board;
-    }
+  /**
+   * Returns this Model's board.
+   */
+  public Board getBoard() {
+    return board;
+  }
 
-    /** Returns true if at least one space on the Board is empty.
-     *  Empty spaces are stored as null.
-     * */
-    public boolean emptySpaceExists() {
-        // TODO: Task 2. Fill in this function.
-        for (int i = 0; i < this.size(); i++) {
-            for (int j = 0; j < this.size(); j++) {
-                Tile currentTile = this.tile(i, j);
-                try {
-                    int tileValue = currentTile.value();
-                    if (tileValue == MAX_PIECE) {
-                        this.setMaxTileExists(true);
-                    }
-                } catch(NullPointerException e) {
-                    return true;
-                }
-            }
+  /**
+   * Returns true if at least one space on the Board is empty.
+   * Empty spaces are stored as null.
+   */
+  public boolean emptySpaceExists() {
+    // TODO: Task 2. Fill in this function.
+    for (int i = 0; i < this.size(); i++) {
+      for (int j = 0; j < this.size(); j++) {
+        Tile currentTile = this.tile(i, j);
+        try {
+          int tileValue = currentTile.value();
+          if (tileValue == MAX_PIECE) {
+            this.setMaxTileExists(true);
+          }
+        } catch (NullPointerException e) {
+          return true;
         }
-        return false;
+      }
     }
+    return false;
+  }
 
-    /**
-     * Returns true if any tile is equal to the maximum valid value.
-     * Maximum valid value is given by this.MAX_PIECE. Note that
-     * given a Tile object t, we get its value with t.value().
-     */
-    public boolean maxTileExists() {
-        // TODO: Task 3. Fill in this function.
-        if (this.maxTileExists)
+  /**
+   * Returns true if any tile is equal to the maximum valid value.
+   * Maximum valid value is given by this.MAX_PIECE. Note that
+   * given a Tile object t, we get its value with t.value().
+   */
+  public boolean maxTileExists() {
+    // TODO: Task 3. Fill in this function.
+    if (this.maxTileExists)
+      return true;
+    for (int i = 0; i < this.size(); i++) {
+      for (int j = 0; j < this.size(); j++) {
+        Tile currentTile = this.tile(i, j);
+        try {
+          int tileValue = currentTile.value();
+          if (tileValue == MAX_PIECE) {
+            this.setMaxTileExists(true);
             return true;
-        for (int i = 0; i < this.size(); i++) {
-            for (int j = 0; j < this.size(); j++) {
-                Tile currentTile = this.tile(i, j);
-                try {
-                    int tileValue = currentTile.value();
-                    if (tileValue == MAX_PIECE) {
-                        this.setMaxTileExists(true);
-                        return true;
-                    }
-                } catch(NullPointerException e) {
-                    // do nothing
-                }
-            }
+          }
+        } catch (NullPointerException e) {
+          // do nothing
         }
-        return false;
+      }
     }
+    return false;
+  }
 
-    /**
-     * Returns true if there are any valid moves on the board.
-     * There are two ways that there can be valid moves:
-     * 1. There is at least one empty space on the board.
-     * 2. There are two adjacent tiles with the same value.
-     */
-    public boolean atLeastOneMoveExists() {
-        // TODO: Fill in this function.
-        if (emptySpaceExists())
+  /**
+   * Returns true if there are any valid moves on the board.
+   * There are two ways that there can be valid moves:
+   * 1. There is at least one empty space on the board.
+   * 2. There are two adjacent tiles with the same value.
+   */
+  public boolean atLeastOneMoveExists() {
+    // TODO: Fill in this function.
+    if (emptySpaceExists())
+      return true;
+    List<Tile> adjacentTiles;
+    for (int i = 0; i < this.size(); i++) {
+      for (int j = 0; j < this.size(); j++) {
+        int currentTileVal = this.tile(i, j).value();
+        adjacentTiles = this.getAdjacentTiles(i, j);
+        for (Tile tile : adjacentTiles) {
+          if (tile.value() == currentTileVal)
             return true;
-        List<Tile> adjacentTiles;
-        for (int i = 0; i < this.size(); i++) {
-            for (int j = 0; j < this.size(); j++) {
-                int currentTileVal = this.tile(i, j).value();
-                adjacentTiles = this.getAdjacentTiles(i, j);
-                for (Tile tile : adjacentTiles) {
-                    if (tile.value() == currentTileVal)
-                        return true;
-                }
-            }
         }
-        return false;
+      }
     }
+    return false;
+  }
 
-    /**
-     * Given a set of x, y coordinates of a tile, return the list of tiles adjacent
-     * @param tileX
-     * @param tileY
-     * @return
-     */
-    private List<Tile> getAdjacentTiles(int tileX, int tileY) {
-        List<Tile> adjacentTiles = new ArrayList<Tile>();
-        int leftTileX = tileX - 1;
-        if (leftTileX >= 0)
-            adjacentTiles.add(this.tile(leftTileX, tileY));
-        int rightTileX = tileX + 1;
-        if (rightTileX < this.size())
-            adjacentTiles.add(this.tile(rightTileX, tileY));
-        int topTileY = tileY -1;
-        if (topTileY >= 0)
-            adjacentTiles.add(this.tile(tileX, topTileY));
-        int bottomTileY = tileY + 1;
-        if (bottomTileY < this.size())
-            adjacentTiles.add(this.tile(tileX, bottomTileY));
-        return adjacentTiles;
-    }
+  /**
+   * Given a set of x, y coordinates of a tile, return the list of tiles adjacent
+   *
+   * @param tileX
+   * @param tileY
+   * @return
+   */
+  private List<Tile> getAdjacentTiles(int tileX, int tileY) {
+    List<Tile> adjacentTiles = new ArrayList<Tile>();
+    int leftTileX = tileX - 1;
+    if (leftTileX >= 0)
+      adjacentTiles.add(this.tile(leftTileX, tileY));
+    int rightTileX = tileX + 1;
+    if (rightTileX < this.size())
+      adjacentTiles.add(this.tile(rightTileX, tileY));
+    int topTileY = tileY - 1;
+    if (topTileY >= 0)
+      adjacentTiles.add(this.tile(tileX, topTileY));
+    int bottomTileY = tileY + 1;
+    if (bottomTileY < this.size())
+      adjacentTiles.add(this.tile(tileX, bottomTileY));
+    return adjacentTiles;
+  }
 
-    /**
-     * Moves the tile at position (x, y) as far up as possible.
-     *
-     * Rules for Tilt:
-     * 1. If two Tiles are adjacent in the direction of motion and have
-     *    the same value, they are merged into one Tile of twice the original
-     *    value and that new value is added to the score instance variable
-     * 2. A tile that is the result of a merge will not merge again on that
-     *    tilt. So each move, every tile will only ever be part of at most one
-     *    merge (perhaps zero).
-     * 3. When three adjacent tiles in the direction of motion have the same
-     *    value, then the leading two tiles in the direction of motion merge,
-     *    and the trailing tile does not.
-     */
-    public void moveTileUpAsFarAsPossible(int x, int y) {
-        Tile currTile = board.tile(x, y);
-        int myValue = currTile.value();
-        int targetY = y;
+  /**
+   * Moves the tile at position (x, y) as far up as possible.
+   * <p>
+   * Rules for Tilt:
+   * 1. If two Tiles are adjacent in the direction of motion and have
+   * the same value, they are merged into one Tile of twice the original
+   * value and that new value is added to the score instance variable
+   * 2. A tile that is the result of a merge will not merge again on that
+   * tilt. So each move, every tile will only ever be part of at most one
+   * merge (perhaps zero).
+   * 3. When three adjacent tiles in the direction of motion have the same
+   * value, then the leading two tiles in the direction of motion merge,
+   * and the trailing tile does not.
+   */
+  public void moveTileUpAsFarAsPossible(int x, int y) {
+    Tile currTile = board.tile(x, y);
+    int myValue = currTile.value();
+    int targetY = y;
 
-        // TODO: Tasks 5, 6, and 10. Fill in this function.
-    }
+    // TODO: Tasks 5, 6, and 10. Fill in this function.
 
-    /** Handles the movements of the tilt in column x of the board
-     * by moving every tile in the column as far up as possible.
-     * The viewing perspective has already been set,
-     * so we are tilting the tiles in this column up.
-     * */
-    public void tiltColumn(int x) {
-        // TODO: Task 7. Fill in this function.
-    }
-
-    public void tilt(Side side) {
-        // TODO: Tasks 8 and 9. Fill in this function.
-    }
-
-    /** Tilts every column of the board toward SIDE.
-     */
-    public void tiltWrapper(Side side) {
-        board.resetMerged();
-        tilt(side);
-    }
-
-
-    @Override
-    public String toString() {
-        Formatter out = new Formatter();
-        out.format("%n[%n");
-        for (int y = size() - 1; y >= 0; y -= 1) {
-            for (int x = 0; x < size(); x += 1) {
-                if (tile(x, y) == null) {
-                    out.format("|    ");
-                } else {
-                    out.format("|%4d", tile(x, y).value());
-                }
-            }
-            out.format("|%n");
+    // Move tiles up
+    for (int i = y + 1; i < this.size(); i++) {
+      try {
+        Tile nextTile = this.tile(x, i);
+        int nextTileValue = nextTile.value();
+        if (!currTile.wasMerged() && !nextTile.wasMerged() && myValue == nextTileValue) {
+          targetY++;
+          this.score += 2*myValue;
         }
-        String over = gameOver() ? "over" : "not over";
-        out.format("] %d (game is %s) %n", score(), over);
-        return out.toString();
+      } catch (NullPointerException e) {
+        targetY++;
+      }
     }
+    if (targetY != y) {
+      board.move(x, targetY, currTile);
+    }
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        return (o instanceof Model m) && this.toString().equals(m.toString());
-    }
+  /**
+   * Handles the movements of the tilt in column x of the board
+   * by moving every tile in the column as far up as possible.
+   * The viewing perspective has already been set,
+   * so we are tilting the tiles in this column up.
+   */
+  public void tiltColumn(int x) {
+    // TODO: Task 7. Fill in this function.
+    for (int y = this.size() - 1; y >= 0; y--) {
+      try {
+        this.tile(x, y).value();
+        moveTileUpAsFarAsPossible(x, y);
+      } catch (NullPointerException e) {
+        // do nothing
+      }
 
-    @Override
-    public int hashCode() {
-        return toString().hashCode();
     }
+  }
+
+  public void tilt(Side side) {
+    // TODO: Tasks 8 and 9. Fill in this function.
+    board.setViewingPerspective(side);
+    for (int x = 0; x < this.size(); x++) {
+      tiltColumn(x);
+    }
+    board.setViewingPerspective(Side.NORTH);
+  }
+
+  /**
+   * Tilts every column of the board toward SIDE.
+   */
+  public void tiltWrapper(Side side) {
+    board.resetMerged();
+    tilt(side);
+  }
+
+
+  @Override
+  public String toString() {
+    Formatter out = new Formatter();
+    out.format("%n[%n");
+    for (int y = size() - 1; y >= 0; y -= 1) {
+      for (int x = 0; x < size(); x += 1) {
+        if (tile(x, y) == null) {
+          out.format("|    ");
+        } else {
+          out.format("|%4d", tile(x, y).value());
+        }
+      }
+      out.format("|%n");
+    }
+    String over = gameOver() ? "over" : "not over";
+    out.format("] %d (game is %s) %n", score(), over);
+    return out.toString();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    return (o instanceof Model m) && this.toString().equals(m.toString());
+  }
+
+  @Override
+  public int hashCode() {
+    return toString().hashCode();
+  }
 }
